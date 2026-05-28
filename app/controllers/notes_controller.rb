47318549f8142512274eec006279ae43c1919b93
Your Name <you@example.com>
2026-05-28 16:17:23 +0900

    学習ノート編集機能を追加

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -18,6 +18,20 @@ class NotesController < ApplicationController
     end
   end
 
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(note_params)
+      redirect_to notes_path, notice: "学習ノートを更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def note_params
